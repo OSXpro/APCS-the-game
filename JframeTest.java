652aa42638c55d5fe6be544e99a1f04ca471fc6a
OSXpro <tmittal@student.naperville203.org>
2016-04-29 13:41:28 -0500

    Update JframeTest.java

--- JframeTest.java
+++ JframeTest.java
@@ -5,16 +5,26 @@ import java.io.File;
 import java.io.IOException;
 
 
-public class JframeTest extends JPanel {
+public class JframeTest extends JPanel implements KeyListener{]{
+	Sprite character;
+	Set<Character> keys = new HashSet<Character>();
+	
 	public JframeTest(){
 		super();
-		setBackground(Color.white);
+		try {
+			character = new Sprite(400,250, 1, ImageIO.read(new File("H:/APCS/Game/src/Sprite.jpg")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
+	
 	public Dimension getPreferredSize() {
 	    return new Dimension(800, 500);
 	}
+	
 	public static void main(String[] args){
-		JPanel panel = new JPanel();
+	    JPanel panel = new JPanel();
 	    JFrame frame = new JFrame("APCS the game");
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.add(new JframeTest());
@@ -23,18 +33,32 @@ public class JframeTest extends JPanel {
 	    frame.setBackground(Color.white);
 	    frame.setVisible(true);
 	}
+	
 	protected void paintComponent(Graphics g) {
-		setBackground(Color.white);
+	    setBackground(Color.white);
 	    int width = getWidth();
 	    int height = getHeight();
 	    g.setColor(Color.black);
 	    g.drawOval(0, 0, width, height);
-	    Toolkit toolkit = Toolkit.getDefaultToolkit();
-	    try {
-			g.drawImage(ImageIO.read(new File("H:/APCS/Game/src/Sprite.jpg")), 100, 100, null);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	    character.draw(g);
+	    character.move(keys);
 	    repaint();
 	  }
+	  
+	public synchronized void keyPressed(KeyEvent event) {
+		//Adds pressed key to set
+		keys.add(event.getKeyChar());
+	}
+		
+	//Runs when a key is released
+	public synchronized void keyReleased(KeyEvent event){
+		//Method to remove key from Set if released
+		if(keys.contains(event.getKeyChar()))
+			keys.remove(event.getKeyChar());
+	}
+		
+	//Empty Implemented method that has to be here for use of listener
+	public void keyTyped(KeyEvent event) {
+			
+	}
 }
