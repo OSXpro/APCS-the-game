edae5cfbbda87d5adc0d42d3bbd0588e436b7bcf
Liberated-Nugget <mpsabato98@gmail.com>
2016-05-05 10:06:04 -0500

    Added files via upload

---
+++ playCutOne.java
@@ -0,0 +1,34 @@
+import java.awt.event.WindowEvent;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class playCutOne {
+	public static void main(String[] args){
+
+	  
+	    Icon icon = new ImageIcon("H:/Final pictures/Cutscene11.gif");
+	    JLabel label = new JLabel(icon);
+
+	    JFrame f = new JFrame("Animation");
+	    f.getContentPane().add(label);
+	    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    f.pack();
+	    f.setLocationRelativeTo(null);
+	    f.setVisible(true);
+	    
+	    try {
+			Thread.sleep(28026);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    f.dispose();
+	    //processEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
+	}
+}
