c7ca83c8131356fbfb4c85586a61b2d88c5826cf
OSXpro <tmittal@student.naperville203.org>
2016-05-12 10:16:46 -0500

    Update InfiniteLoop.java

--- InfiniteLoop.java
+++ InfiniteLoop.java
@@ -5,46 +5,55 @@ import java.io.File;
 import java.io.IOException;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-public class InfinateLoop extends JPanel implements KeyListener{
+public class InfiniteLoop extends JPanel implements KeyListener{
 	public Sprite character;
 	Set<Character> keys = new HashSet<Character>();
-	static JTextField t;
+	ArrayList<Image> sprite = new ArrayList<Image>();
 	int frames;
-	
-	public InfinateLoop(){
+	public InfiniteLoop(){
 		super();
+		frames = 0;
 		addKeyListener(this);
 		try {
-			character = new Sprite(400,250, .25, ImageIO.read(new File("src/Sprites/Idle.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/back.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/back walk 1.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/back walk 2.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/left idle.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/left walk.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/left walk 2.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/Idle.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/front walk 1.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/front walk 2.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/right idle.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/right walk.png")));
+			sprite.add(ImageIO.read(new File("src/Sprite/right walk 2.png")));
+			character = new Sprite(400,250, 0.25, sprite);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public Dimension getPreferredSize() {
 	    return new Dimension(800, 500);
-	    
 	}
-	
+
 	public static void main(String[] args){
-	    InfinateLoop panel = new InfinateLoop();
+	    InfiniteLoop panel = new InfiniteLoop();
 	    JFrame frame = new JFrame("APCS the game");
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.add(panel);
 	    frame.addKeyListener(panel);
 	    frame.setSize(800, 500);
-	    t = new JTextField("count ++;");
 	    frame.pack();
 	    frame.setBackground(Color.white);
 	    frame.setVisible(true);
 	}
-	
+
 	public void paintComponent(Graphics g) {
 		
 		frames ++;
@@ -67,23 +76,24 @@ public class InfinateLoop extends JPanel implements KeyListener{
 	    character.move(keys, width, height);
 	    repaint();
 	  }
-	  
+
 	public synchronized void keyPressed(KeyEvent event) {
-		
+		//System.out.println("Hi");
 		//Adds pressed key to set
 		keys.add(event.getKeyChar());
-		System.out.println(keys);
+
 	}
-		
+
 	//Runs when a key is released
 	public synchronized void keyReleased(KeyEvent event){
 		//Method to remove key from Set if released
+		System.out.println(event.getKeyChar());
 		if(keys.contains(event.getKeyChar()))
 			keys.remove(event.getKeyChar());
 	}
-		
+
 	//Empty Implemented method that has to be here for use of listener
 	public void keyTyped(KeyEvent event) {
-			
+
 	}
 }
