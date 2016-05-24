d8c6012cc26c6007c17077e49aad4a2b53e2e4c4
OSXpro <tmittal@student.naperville203.org>
2016-05-24 10:10:27 -0500

    Update InfiniteLoop.java

--- InfiniteLoop.java
+++ InfiniteLoop.java
@@ -1,3 +1,5 @@
+//Tanveer Mittal, Michael Mardyla, Matteo Sabato
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -12,7 +14,9 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+//Inherits Jpanel to easily use many methods
 public class InfiniteLoop extends JPanel implements KeyListener{
+	//Initialize data for this level
 	public Sprite character;
 	Set<Character> keys = new HashSet<Character>();
 	ArrayList<Image> sprite = new ArrayList<Image>();
@@ -24,10 +28,12 @@ public class InfiniteLoop extends JPanel implements KeyListener{
 	String input = "temp";
 	static Music player;
 	
+	//Constructor for our child of Jpanel
 	public InfiniteLoop(){
 		super();
 		frames = 0;
 		addKeyListener(this);
+		//Adds sprite walking images to arraylist in specific order
 		try {
 			sprite.add(ImageIO.read(new File("src/Sprite/back.png")));
 			sprite.add(ImageIO.read(new File("src/Sprite/back walk 1.png")));
@@ -48,12 +54,13 @@ public class InfiniteLoop extends JPanel implements KeyListener{
 
 	}
 	
+	//Method that returns size when frame.pack() is called
 	public Dimension getPreferredSize() {
 	    return new Dimension(800, 500);
 	}
 
 	public static void main(String[] args){
-	    
+	    //Intializes frame and sets up panel as drawing surface
 	    frame = new JFrame("APCS the game");
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.add(panel);
@@ -63,39 +70,41 @@ public class InfiniteLoop extends JPanel implements KeyListener{
 	    frame.pack();
 	    frame.setBackground(Color.white);
 	    frame.setVisible(true);
+	    //Uses our music player class to play song
 	    player = new Music();
-		player.play("src/music/Undertale OST - Temmie Village Extended.wav");
+	    player.play("src/music/Undertale OST - Temmie Village Extended.wav");
 	}
-
+	
+	//
 	public void paintComponent(Graphics g) {
-		
-		frames ++;
-		if(frames > 500){
-			character.move(400, 250);
-			frames = 0;
-		}
+	    //Logic to automatically return character to middle of screen
+   	    frames ++;
+     	    if(frames > 500){
+		character.move(400, 250);
+		frames = 0;
+	}
 	    
-	    
+	    //Draws all parts of level(sprite, text)
 	    int width = getWidth();
 	    int height = getHeight();
 	    g.clearRect(0, 0, width, height);
 	    g.setColor(Color.black);
 	    
 	    g.setFont(new Font("Comic Sans MS", Font.PLAIN, 32)); 
-	    g.drawString("while(1 == 1){", 150, 100);
-		g.drawString("}", 600, 400);
+	    g.drawString("while(count == 1){", 150, 100);
+	    g.drawString("}", 600, 400);
 	    //g.drawOval(0, 0, width, height);
 	    character.draw(g);
 	    
 	    character.move(keys, width, height);
-	    //Closes windows
+	    //Checks for textfield input
 	    if(input.equals("break;") || input.equals("count ++;")){
+	    	//waits 2 seconds then closes window
 	    	try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	    	frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 	    }
 	    else
@@ -103,28 +112,29 @@ public class InfiniteLoop extends JPanel implements KeyListener{
 	  }
 	
 	public synchronized void keyPressed(KeyEvent event) {
-		//System.out.println("Hi");
 		//Adds pressed key to set
 		keys.add(event.getKeyChar());
+		//If enter is pressed, it creates the textfield
 		if(event.getKeyCode() == event.VK_ENTER && !added){
 			txt.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e){
 					input = txt.getText();
 				}
-			});
-			panel.add(txt);
-		    //frame.setSize(800, 500);
-		    frame.pack();
-		    frame.setBackground(Color.white);
-		    frame.setVisible(true);
-			added = true;
+		});
+		//adds field to panel
+		panel.add(txt);
+		
+		//Sets frame for use again
+		frame.pack();
+		frame.setBackground(Color.white);
+		frame.setVisible(true);
+		added = true;
 		}
 	}
 
 	//Runs when a key is released
 	public synchronized void keyReleased(KeyEvent event){
 		//Method to remove key from Set if released
-		System.out.println(event.getKeyChar());
 		if(keys.contains(event.getKeyChar()))
 			keys.remove(event.getKeyChar());
 	}
